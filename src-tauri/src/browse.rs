538ddfc70dcb3961db9bd2de490088d9cb389b7b
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-04-26 09:32:04 +0530

    Removed the string for path and now just files

--- src-tauri/src/browse.rs
+++ src-tauri/src/browse.rs
@@ -2,16 +2,15 @@ use std::fs;
 #[tauri::command]
 pub async fn browse_direcotries() -> Result<(), String> {
     let directories_iterator = fs::read_dir(".").expect("Unable to read current directory");
-    let directories = Vec::new();
-    let files = Vec::new();
+    let mut directories = Vec::new();
+    let mut files = Vec::new();
     for dirtectory in directories_iterator {
         let dir = dirtectory.unwrap();
         if dir.path().is_dir() {
-            directories.push(dir.file_name().into_string().unwrap());
+            directories.push(dir.file_name());
         } else {
-            files.push(dir.file_name().into_string().unwrap());
+            files.push(dir.file_name());
         }
     }
-    let directories = Vec::new();
     Ok(())
 }
